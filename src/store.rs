7c42aba10f519c0b09164458b4c884032390243f
PriyanshuUpadhyay <puofficialid@gmail.com>
2026-09-11 12:42:59 +0530

    store: add finish_job running to done

--- src/store.rs
+++ src/store.rs
@@ -51,6 +51,14 @@ pub fn claim_job(connection: &Connection, job_id: i64) -> Result<bool, Box<dyn s
     Ok(changed == 1)
 }
 
+pub fn finish_job(connection: &Connection, job_id: i64) -> Result<bool, Box<dyn std::error::Error>> {
+    let changed = connection.execute(
+        "UPDATE job SET state = 'done' WHERE id = ?1 AND state = 'running'",
+        [job_id],
+    )?;
+    Ok(changed == 1)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
